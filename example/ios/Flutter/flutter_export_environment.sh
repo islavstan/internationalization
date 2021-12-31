bcb9ebf258d3492e04b5c301c6f1d7b0c56dcf13
Горобец Станислав Олегович <stanislav.gorobets@privatbank.ua>
2021-12-31 21:22:56 +0200

    some fix

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/tercyostorck/snap/flutter/common/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/tercyostorck/Documents/Libraries/internationalization/example"
+export "FLUTTER_ROOT=/Users/stanyslav/Desktop/flutter_2_5_3"
+export "FLUTTER_APPLICATION_PATH=/Users/stanyslav/StudioProjects/internationalization2/example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
